46a66c194adb31ef426689bef578e9cb029fa258
Mr.Hide <draxs633@gmail.com>
2024-02-24 13:23:54 -0600

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; //
+namespace RazorPageMovie.Models
+{
+    public class Movie
+    { 
+        public int Id { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string? Title { get; set; } //este atributo puede ser nulo //data type + ?
+        
+        [Display(Name = "Realese Date")]
+        [DataType(DataType.Date)] 
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
+        public string? Genre { get; set; }
+        
+        [Column(TypeName = "decimal(18,2)")]
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        public decimal price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s]*$")]
+        [StringLength(5)]
+        [Required]
+        public string? Rating { get; set; }  
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,64 @@
+﻿//using Humanizer.Localisation;
+using Microsoft.EntityFrameworkCore;
+using RazorPageMovie.Data;
+using System;
+
+namespace RazorPageMovie.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPageMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<RazorPageMovieContext>>()))
+            {
+                if(context == null || context.Movie == null)  
+                { 
+                    throw new ArgumentException("Null RazorPagesMoviecontext"); 
+                } 
+                //Para mirar cualquier pelicula 
+                if(context.Movie.Any())
+                {
+                    return; //Db muestra todo lo que contiene la clase
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        price = 7.99M, //millions
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbusters",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        price = 8.99M, 
+                        Rating = "G"
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbusters2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        price = 9.99M, 
+                        Rating = "G"
+                    },
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        price = 3.99M,
+                        Rating = "NA"
+                    }
+                    ); 
+                context.SaveChanges();
+            }
+        }
+    }
+}
